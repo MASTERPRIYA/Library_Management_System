dd77704b268952f7fdf6a3fe37614d6849e963f6
Priya Vishwakarma <135530268+MASTERPRIYA@users.noreply.github.com>
2024-03-22 08:03:27 +0530

    Update SMSSender.java

--- src/utils/SMSSender.java
+++ src/utils/SMSSender.java
@@ -5,14 +5,14 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class SMSSender {
-    static final String ACCOUNT_SID = "ACb36d12ee71d1176d8c19f376086bebae";
-    static final String AUTH_TOKEN = "6edda617ec8c9405f5003ef06bfcd518";
+    static final String ACCOUNT_SID = "abc";
+    static final String AUTH_TOKEN = "123";
 
     public static void sendOTP(String phone, String otp) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
             new com.twilio.type.PhoneNumber("+91"+phone),
-                new com.twilio.type.PhoneNumber("+12059527769"),
+                new com.twilio.type.PhoneNumber("+123"),
                 "Welcome to Book Mining Library ! OTP:"+otp)
             .create();
 
